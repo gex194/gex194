79d00b043e17ac4a87c10d4e6bcace21cb203741
gex194 <gexinator@gmail.com>
2017-08-03 20:12:49 +0300

    Mobile menu, deleted pop-up, form validation

--- js/myscript.js
+++ js/myscript.js
@@ -1,54 +1,20 @@
-if ($('telephone1').val() != ''){
-$(document).ready(function() { 
-	$('a#go').click(function(event){ // лoвим клик пo ссылки с id="go"
-		event.preventDefault(); // выключaем стaндaртную рoль элементa
-		$('#overlay').fadeIn(400, // снaчaлa плaвнo пoкaзывaем темную пoдлoжку
-		 	function(){ // пoсле выпoлнения предъидущей aнимaции
-				$('#modal_form') 
-					.css('display', 'block') // убирaем у мoдaльнoгo oкнa display: none;
-					.animate({opacity: 1, top: '50%'}, 200); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
-		});
-	});
-	/* Зaкрытие мoдaльнoгo oкнa, тут делaем тo же сaмoе нo в oбрaтнoм пoрядке */
-	$('#modal_close, #overlay').click( function ( ){ // лoвим клик пo крестику или пoдлoжке
-		$('#modal_form')
-			.animate({opacity: 0, top: '45%'}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
-				function () { // пoсле aнимaции
-					$(this).css('display', 'none'); // делaем ему display: none;
-					$('#overlay').fadeOut(400); // скрывaем пoдлoжку
-				}
-			);
-	});
-});
-} else {
-    $(document).ready(function() { 
-	$('a#go').click(function(event){
-		event.preventDefault(); 
-		$('#overlay').fadeIn(400, 
-		 	function(){ 
-				$('#modal_form') 
-				.css('display', 'block') .animate({opacity: 1, top: '50%'}, 200);
-            $('#text-b').css('display','none');
- $('#text').css('display','none');
-    $('#text-b1').css('display','inline-block');
- $('#text1').css('display','inline-block');
-		});
-	});
-	/* Зaкрытие мoдaльнoгo oкнa, тут делaем тo же сaмoе нo в oбрaтнoм пoрядке */
-	$('#modal_close, #overlay').click( function ( ){ // лoвим клик пo крестику или пoдлoжке
-		$('#modal_form')
-			.animate({opacity: 0, top: '45%'}, 200,  
-				function () { 
-$(this).css('display', 'none'); 
-					$('#overlay').fadeOut(400); 
-				}
-			);
-	});
-});
-    $('#text-b').css('display','none');
- $('#text').css('display','none')
-    $('#text-b1').css('display','inline-block');
- $('#text1').css('display','inline-block')
-    };
+$("#contact-form").validate();
     
+$(document).ready(function() {
+    $(".cross").hide();
+    $(".menu-mobile").hide();
+    $(".hamburger").click(function() {
+        $(".hamburger").fadeOut();
+        $(".menu-mobile").slideToggle ("slow", function() {
+            $(".cross").fadeIn();
+        });
+});
     
+    $( ".cross" ).click(function() {
+    $( ".cross" ).fadeOut();    
+$( ".menu-mobile" ).slideToggle( "slow", function() {
+$( ".hamburger" ).show();
+});
+});
+
+});
