5404a745d4bca2718e0e858f579d22f86e6118e5
gex194 <gexinator@gmail.com>
2017-09-17 15:19:45 +0300

    Fixing Sedona

--- Sedona_project/js/myscript.js
+++ Sedona_project/js/myscript.js
@@ -1,20 +1,64 @@
-$("#contact-form").validate();
-    
+ $("#contact-form").validate();   
 $(document).ready(function() {
     $(".cross").hide();
     $(".menu-mobile").hide();
     $(".hamburger").click(function() {
         $(".hamburger").fadeOut();
-        $(".menu-mobile").slideToggle ("slow", function() {
+        $(".menu-mobile").fadeToggle ("swing", function() {
             $(".cross").fadeIn();
         });
 });
     
     $( ".cross" ).click(function() {
     $( ".cross" ).fadeOut();    
-$( ".menu-mobile" ).slideToggle( "slow", function() {
+$( ".menu-mobile" ).fadeToggle( "swing", function() {
 $( ".hamburger" ).show();
 });
 });
 
+});
+
+$(document).ready(function() {
+    $("#e_dialog").dialog({
+        modal:true,
+        autoOpen:false,
+        width:600,
+        height:350,
+        show: {
+            effect:"blind",
+            duration:500
+        },
+        hide: {
+            effect:"blind",
+            duration:500
+        },
+        buttons: {
+            "Close": function(){
+                $("#e_dialog").dialog('close');
+            }
+        }
+    });
+    $("#dialog").dialog({
+        modal: true,
+        autoOpen: false,
+        width: 600,
+        height: 350,
+        show: {
+        effect:"blind",
+            duration: 500
+    },
+        hide:{
+            effect:"blind",
+            duration:500
+        },
+        buttons: {
+            "Close": function(){
+                $("#dialog").dialog('close');
+            }
+        }
+    });
+    
+    $("#go").click(function() {
+        $("#dialog").dialog('open');
+    });
 });
